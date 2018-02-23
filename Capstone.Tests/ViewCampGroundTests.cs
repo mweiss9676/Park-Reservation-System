d79e87c66107709c0a47f41f2d321f346acf1b8c
Jimmy Vanetta <j_vanetta@yahoo.com>
2018-02-23 09:56:27 -0500

    pushing dem tests me bredda

---
+++ Capstone.Tests/ViewCampGroundTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data.Sql;
+using System.Collections.Generic;
+using System.Configuration;
+using Capstone.DAL;
+using Capstone.Menus;
+using Capstone.Models;
+
+namespace Capstone.Tests
+{
+    [TestClass]
+    public class ViewCampGroundTests
+    {
+        static string connectionString = @"Server=.\SQLEXPRESS;Database=NationalParks;Trusted_Connection=True";
+
+        [TestMethod]
+        public void GetCampGrounds_CountList_Test()
+        {
+            ViewParksDAL v = new ViewParksDAL();
+            CampgroundDAL c = new CampgroundDAL();
+
+            Park p = v.GetParkByName("Acadia", connectionString);
+            Park p2 = v.GetParkByName("Arches", connectionString);
+            Park p3 = v.GetParkByName("Cuyahoga Valley", connectionString);
+
+            List<Campground>cg = c.GetCampgrounds(p, connectionString);
+            List<Campground> cg2 = c.GetCampgrounds(p2, connectionString);
+            List<Campground> cg3 = c.GetCampgrounds(p3, connectionString);
+
+            int result = cg.Count;
+            int result2 = cg2.Count;
+            int result3 = cg3.Count;
+
+            Assert.AreEqual(result, 3);
+            Assert.AreEqual(result2, 3);
+            Assert.AreEqual(result3, 1);
+        }
+    }
+}
---
+++ Capstone.Tests/ViewCampSiteTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data.Sql;
+using System.Collections.Generic;
+using System.Configuration;
+using Capstone.DAL;
+using Capstone.Menus;
+using Capstone.Models;
+
+namespace Capstone.Tests
+{
+    [TestClass]
+    public class ViewCampSiteTests
+    {
+        static string connectionString = @"Server=.\SQLEXPRESS;Database=NationalParks;Trusted_Connection=True";
+
+        [TestMethod]
+        public void GetCampSites_CountList_Test()
+        {
+            ViewParksDAL v = new ViewParksDAL();
+            CampgroundDAL c = new CampgroundDAL();
+            CampsiteDAL s = new CampsiteDAL();
+
+            Park p = v.GetParkByName("Acadia", connectionString);
+
+            List<Campground> cglist = c.GetCampgrounds(p, connectionString);
+
+            Campground cg = cglist[0];
+
+            List<Campsite> output = s.GetCampsites(cg, connectionString);
+
+            int result = output.Count;
+
+            Assert.AreEqual(result, 12);
+        }
+    }
+}
